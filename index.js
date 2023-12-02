89a49427240fecea6e2539fd1390ee5bd40e5276
emma-landry7 <emmablandry730@gmail.com>
2023-12-01 23:16:47 -0600

    modified bootstrap, stuck on navbar dropdown menu

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+function dropdown() {
 axios.get("https://fakestoreapi.com/products")
     .then((response) => {
         console.log(response.data)
@@ -22,7 +23,62 @@ axios.get("https://fakestoreapi.com/products")
             })
         }
 
-        getData().then((data) => addCard(data))
+        // getData().then((data) => addCard(data))
+        const mensLink = document.getElementById('mens-clothing')
+        const womensLink = document.getElementById('womens-clothing')
+        const jewelryLink = document.getElementById('jewelry')
+        const electronicsLink = document.getElementById('electronics')
+        console.log(mensLink)
 
+        // const dropdownMenu = document.getElementById('dropdown-menu')
+        // function dropdown() {
+            response.data.forEach((item) => {
+                if (item.category === "men's clothing" && mensLink) {
+                    mens = item
+                    console.log(item)
+                    const template = document.getElementById('card-template').content.cloneNode(true);
+                    template.querySelector('.card-image').setAttribute("src", item.image);
+                    template.querySelector('.card-title').innerText = item.title;
+                    template.querySelector('.card-price').innerText = item.price;
+                    template.querySelector('.card-text').innerText = item.description;
+
+                    
+                    document.querySelector('#product-list').appendChild(template)
+                    // getData().then((data) => addCard(data))
+                } else if (item.category === "women's clothing" && womensLink) {
+                    // console.log(item)
+                    const template = document.getElementById('card-template').content.cloneNode(true);
+                    template.querySelector('.card-image').setAttribute("src", item.image);
+                    template.querySelector('.card-title').innerText = item.title;
+                    template.querySelector('.card-price').innerText = item.price;
+                    template.querySelector('.card-text').innerText = item.description;
+                    // document.querySelector('#product-list').removeChild()
+                    document.querySelector('#product-list').appendChild(template)
+                }
+                if (item.category === "jewelry" && jewelryLink) {
+                    // console.log(item)
+                    const template = document.getElementById('card-template').content.cloneNode(true);
+                    template.querySelector('.card-image').setAttribute("src", item.image);
+                    template.querySelector('.card-title').innerText = item.title;
+                    template.querySelector('.card-price').innerText = item.price;
+                    template.querySelector('.card-text').innerText = item.description;
+
+                    document.querySelector('#product-list').appendChild(template)
+                }
+                if (item.category === "electronics" && electronicsLink) {
+                    // console.log(item)
+                    const template = document.getElementById('card-template').content.cloneNode(true);
+                    template.querySelector('.card-image').setAttribute("src", item.image);
+                    template.querySelector('.card-title').innerText = item.title;
+                    template.querySelector('.card-price').innerText = item.price;
+                    template.querySelector('.card-text').innerText = item.description;
+
+                    document.querySelector('#product-list').appendChild(template)
+                }
+            })
+        // }
+        // dropdown()
+        
     })
 
+}
